1752654ad799f4cae2332c042650d113f7be74dc
Marcos5399 <maarcosangel2@gmail.com>
2025-04-12 13:06:39 +0200

    Initial commit

---
+++ src/main/java/integrationProjectGHM/GitHubMiner/model/commit/Commit.java
@@ -0,0 +1,202 @@
+
+package integrationProjectGHM.GitHubMiner.model.commit;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Commit {
+
+    @JsonProperty("url") // Consituye a web_url de GitMiner
+    private String url;
+    // OJO !! --> En la URL de los Commits se encuentra el nombre del repositorio, por lo que podemos relacionar Commits con Projects por ahí
+    @JsonProperty("sha") // Consituye a id de GitMiner
+    private String sha;
+    @JsonProperty("node_id")
+    private String nodeId;
+    @JsonProperty("html_url")
+    private String htmlUrl;
+    @JsonProperty("comments_url")
+    private String commentsUrl;
+    @JsonProperty("commit")
+    private Commit__1 commit;
+    @JsonProperty("author")
+    private Author author;
+    @JsonProperty("committer")
+    private Committer__1 committer;
+    @JsonProperty("parents")
+    private List<Parent> parents;
+    @JsonProperty("stats")
+    private Stats stats;
+    @JsonProperty("files")
+    private List<File> files;
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @JsonProperty("sha")
+    public String getSha() {
+        return sha;
+    }
+
+    @JsonProperty("sha")
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+    @JsonProperty("node_id")
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    @JsonProperty("node_id")
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    @JsonProperty("html_url")
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    @JsonProperty("html_url")
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    @JsonProperty("comments_url")
+    public String getCommentsUrl() {
+        return commentsUrl;
+    }
+
+    @JsonProperty("comments_url")
+    public void setCommentsUrl(String commentsUrl) {
+        this.commentsUrl = commentsUrl;
+    }
+
+    @JsonProperty("commit")
+    public Commit__1 getCommit() {
+        return commit;
+    }
+
+    @JsonProperty("commit")
+    public void setCommit(Commit__1 commit) {
+        this.commit = commit;
+    }
+
+    @JsonProperty("author")
+    public Author getAuthor() {
+        return author;
+    }
+
+    @JsonProperty("author")
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @JsonProperty("committer")
+    public Committer__1 getCommitter() {
+        return committer;
+    }
+
+    @JsonProperty("committer")
+    public void setCommitter(Committer__1 committer) {
+        this.committer = committer;
+    }
+
+    @JsonProperty("parents")
+    public List<Parent> getParents() {
+        return parents;
+    }
+
+    @JsonProperty("parents")
+    public void setParents(List<Parent> parents) {
+        this.parents = parents;
+    }
+
+    @JsonProperty("stats")
+    public Stats getStats() {
+        return stats;
+    }
+
+    @JsonProperty("stats")
+    public void setStats(Stats stats) {
+        this.stats = stats;
+    }
+
+    @JsonProperty("files")
+    public List<File> getFiles() {
+        return files;
+    }
+
+    @JsonProperty("files")
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Commit.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("url");
+        sb.append('=');
+        sb.append(((this.url == null)?"<null>":this.url));
+        sb.append(',');
+        sb.append("sha");
+        sb.append('=');
+        sb.append(((this.sha == null)?"<null>":this.sha));
+        sb.append(',');
+        sb.append("nodeId");
+        sb.append('=');
+        sb.append(((this.nodeId == null)?"<null>":this.nodeId));
+        sb.append(',');
+        sb.append("htmlUrl");
+        sb.append('=');
+        sb.append(((this.htmlUrl == null)?"<null>":this.htmlUrl));
+        sb.append(',');
+        sb.append("commentsUrl");
+        sb.append('=');
+        sb.append(((this.commentsUrl == null)?"<null>":this.commentsUrl));
+        sb.append(',');
+        sb.append("commit");
+        sb.append('=');
+        sb.append(((this.commit == null)?"<null>":this.commit));
+        sb.append(',');
+        sb.append("author");
+        sb.append('=');
+        sb.append(((this.author == null)?"<null>":this.author));
+        sb.append(',');
+        sb.append("committer");
+        sb.append('=');
+        sb.append(((this.committer == null)?"<null>":this.committer));
+        sb.append(',');
+        sb.append("parents");
+        sb.append('=');
+        sb.append(((this.parents == null)?"<null>":this.parents));
+        sb.append(',');
+        sb.append("stats");
+        sb.append('=');
+        sb.append(((this.stats == null)?"<null>":this.stats));
+        sb.append(',');
+        sb.append("files");
+        sb.append('=');
+        sb.append(((this.files == null)?"<null>":this.files));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
